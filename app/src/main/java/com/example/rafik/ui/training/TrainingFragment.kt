3a1a758f8658b956ba7e18d05899f7d7ba54b793
AbdoTalaat74 <abdo.talaat740@gmail.con>
2023-08-22 21:38:33 +0300

    send Training Request

--- app/src/main/java/com/example/rafik/ui/training/TrainingFragment.kt
+++ app/src/main/java/com/example/rafik/ui/training/TrainingFragment.kt
@@ -1,37 +1,97 @@
 package com.example.rafik.ui.training
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.rafik.R
 import com.example.rafik.databinding.FragmentTrainingBinding
 import com.example.rafik.ui.initToolbar
 
-class TrainingFragment: Fragment() {
-    private lateinit var binding:FragmentTrainingBinding
+class TrainingFragment : Fragment() {
+    private lateinit var binding: FragmentTrainingBinding
+    private lateinit var viewModel: TrainingViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = FragmentTrainingBinding.inflate(layoutInflater)
 
-        initToolbar(binding.topAppBar,getString(R.string.trainings),this)
+        viewModel = ViewModelProvider(
+            this,
+            TrainingVMFactory(requireContext().applicationContext as Application)
+        )[TrainingViewModel::class.java]
 
-        val trainingsType = resources.getStringArray(R.array.product_types)
-        val trainingsAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,trainingsType)
+        binding.viewModel = viewModel
 
-        val trainingPlaces = resources.getStringArray(R.array.training_places)
-        val placesAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,trainingPlaces)
+        initToolbar(binding.topAppBar, getString(R.string.trainings), this)
+        initProductTypeSpinner()
+        initTrainingPlacesSpinner()
 
-        binding.productTypeSpinner.adapter = trainingsAdapter
+        viewModel.onNavigateUp.observe(viewLifecycleOwner) {
+            if (it) {
+                findNavController().navigateUp()
+            }
+        }
 
-        binding.trainingPlacesSpinner.adapter = placesAdapter
 
         return binding.root
     }
+
+
+    private fun initProductTypeSpinner() {
+        val trainingsType = resources.getStringArray(R.array.product_types)
+        val trainingsAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, trainingsType)
+        binding.productTypeSpinner.adapter = trainingsAdapter
+        binding.productTypeSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    Log.i("initAreaSpinner", "area: ${trainingsType[position]}")
+                    viewModel.postProductType(trainingsType[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    //todo
+                }
+            }
+    }
+
+    private fun initTrainingPlacesSpinner(){
+        val trainingPlaces = resources.getStringArray(R.array.training_places)
+        val placesAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, trainingPlaces)
+        binding.trainingPlacesSpinner.adapter = placesAdapter
+
+        binding.trainingPlacesSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    Log.i("initAreaSpinner", "area: ${trainingPlaces[position]}")
+                    viewModel.postTrainingPlace(trainingPlaces[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    //todo
+                }
+            }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/rafik/ui/training/TrainingViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.rafik.ui.training
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rafik.data.repo.FireBaseRepoImpl
+import com.example.rafik.domian.entity.TrainingRequest
+import com.example.rafik.domian.entity.User
+import kotlinx.coroutines.launch
+
+class TrainingViewModel(application: Application) : ViewModel() {
+
+
+    private val fireBaseRepoImpl = FireBaseRepoImpl()
+    private val _productType = MutableLiveData<String>()
+    private val _trainingPlace = MutableLiveData<String>()
+    private lateinit var trainingRequest: TrainingRequest
+
+
+    private val _onNavigateUp = MutableLiveData<Boolean>()
+    val onNavigateUp: LiveData<Boolean>
+        get() = _onNavigateUp
+
+    fun postProductType(productType: String) {
+        _productType.postValue(productType)
+    }
+
+    fun postTrainingPlace(trainingPlace: String) {
+        _trainingPlace.postValue(trainingPlace)
+    }
+
+    fun sendRequest() {
+        Log.e("TrainingViewModel","sendRequest called")
+        Log.e("TrainingViewModel",_productType.value!!)
+        Log.e("TrainingViewModel",_trainingPlace.value!!)
+        if (_trainingPlace.value.isNullOrBlank() && _productType.value.isNullOrBlank()) {
+            Log.e("TrainingViewModel","Null values")
+            return
+        }else{
+            if (user.value!=null){
+                trainingRequest = TrainingRequest(
+                    productType = _productType.value!!,
+                    trainingPlace = _trainingPlace.value!!,
+                    user = user.value!!
+                )
+                Log.e("TrainingViewModel","Request sent")
+                _onNavigateUp.value = true
+            }
+        }
+    }
+
+
+    init {
+        viewModelScope.launch {
+            fireBaseRepoImpl.getUser()
+        }
+    }
+
+    val user = fireBaseRepoImpl.user
+}
+
